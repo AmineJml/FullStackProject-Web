42f72495f4f07d172648d41aabe44ea663c68842
AmineJml <aminejamal003@gmail.com>
2022-11-20 13:30:37 +0200

    Completing add_post API

--- Back End/add_post.php
+++ Back End/add_post.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 include("connection.php");
 
-if(isset($_POST["User_id"]) && $_POST["User_id"] != "" && isset($_POST["Image_URL"]) && $_POST["Image_URL"] != "" && isset($_POST["Username"]) && $_POST["Username"] != ""&& isset($_POST["Password"]) && $_POST["Password"] != "" ){
+if(isset($_POST["User_id"]) && $_POST["User_id"] != "" && isset($_POST["Image_URL"]) && $_POST["Image_URL"] != "" ){
     $User_id = $_POST["User_id"];
     $Image_URL = $_POST["Image_URL"];
 
@@ -16,15 +16,15 @@ if(isset($_POST["User_id"]) && $_POST["User_id"] != "" && isset($_POST["Image_UR
      return; 
  }
 
-$query = $mysqli->prepare("INSERT INTO Images Image_URL WHERE User_id = ?");
-$query->bind_param("i", $User_id);
+$query = $mysqli->prepare("INSERT INTO Images(Image_URL, User_id) VALUES(?, ?)");
+$query->bind_param("si",$Image_URL, $User_id);
 $query->execute();
 
 $array = $query->get_result();
 
 $response = [];
 $response["success"] = "success";
-echo json_encode($users);
+echo json_encode($response);
 
 
 
